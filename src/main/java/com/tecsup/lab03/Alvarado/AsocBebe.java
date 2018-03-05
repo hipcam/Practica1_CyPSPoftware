72882269166a1f0b29b2617780bc9c904b90d522
Fernanda <alessandrafernanda.alvarado@tecsup.edu.pe>
2018-03-05 11:49:37 -0500

    metodos

--- src/main/java/com/tecsup/lab03/Alvarado/AsocBebe.java
+++ src/main/java/com/tecsup/lab03/Alvarado/AsocBebe.java
@@ -1,5 +1,22 @@
 package com.tecsup.lab03.Alvarado;
 
 public class AsocBebe {
-
+	
+	String [] pdto1 = {"Casaca", "cuero", "3"};
+	String [] pdto2 = {"Gorro", "cuero", "4"};
+	String [] pdto3 = {"Pantalon", "cuero", "5"};
+	
+    public void publicar(){
+    	System.out.println("BEBE: ");
+    	System.out.println("Prenda 1: ");
+    	System.out.println("Detalles : Nombre"+pdto1[0]+ " Descripción :"+pdto1[1]+ " Talla :"+pdto1[2]);
+    	System.out.println("Prenda 2: ");
+    	System.out.println("Detalles : Nombre"+pdto2[0]+ " Descripción :"+pdto2[1]+ " Talla :"+pdto2[2]);
+    	System.out.println("Prenda 3: ");
+    	System.out.println("Detalles : Nombre"+pdto3[0]+ " Descripción :"+pdto3[1]+ " Talla :"+pdto3[2]);
+    	
+  
+    }
 }
+//-	La asociación de ropas de bebe solo desea publicar los
+//nombres de la prenda, su descripción, talla,  pero no el precio
--- src/main/java/com/tecsup/lab03/Alvarado/AsocDama.java
+++ src/main/java/com/tecsup/lab03/Alvarado/AsocDama.java
@@ -1,5 +1,23 @@
 package com.tecsup.lab03.Alvarado;
 
 public class AsocDama {
-
+	String [] pdto1 = {"Chalina", "cuero", "3", "35.00"};
+	String [] pdto2 = {"Blusa", "seda", "4", "13.00"};
+	String [] pdto3 = {"Botas", "cuero", "5", "90.50"};
+	
+    public void publicar(){
+    	System.out.println("DAMA: ");
+    	System.out.println("Prenda 1: ");
+    	System.out.println("Detalles : Nombre"+pdto1[0]+ " Descripción :"+pdto1[1]+ " Talla :"+pdto1[2]+ " Precio: "+pdto1[3]);
+    	System.out.println("Prenda 2: ");
+    	System.out.println("Detalles : Nombre"+pdto2[0]+ " Descripción :"+pdto2[1]+ " Talla :"+pdto2[2]+ " Precio: "+pdto2[3]);
+    	System.out.println("Prenda 3: ");
+    	System.out.println("Detalles : Nombre"+pdto3[0]+ " Descripción :"+pdto3[1]+ " Talla :"+pdto3[2]+ " Precio: "+pdto3[3]);
+    	
+  
+    }
 }
+
+
+//-	La asociación de ropa de damas desea publicar
+//los nombres de la prenda, su descripción, talla y el precio
